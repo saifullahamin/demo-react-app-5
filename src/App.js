4820a5a2c18b882ccaad4c3a5dfb03be70c2f63a
Saifullah Amin <saifullahaminkhan@gmail.com>
2020-12-01 18:10:13 +0500

    Setup the fetch API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [repos, setRepos] = useState([{}]);
+  useEffect(() => {
+
+    // fetch("https://api.github.com/users/saifullahamin/repos")
+    // .then((response) => response.json())
+    // .then((json) => {
+    //   console.log(json);
+    // });
+
+    async function fetchData() {
+      const respone = await fetch(
+        "https://api.github.com/users/saifullahamin/repos"
+      );
+      const data = await respone.json();
+      setRepos(data);
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {repos.map((repoObj, id) => {
+          return <li key={id}>{repoObj.name}</li>;
+        })}
+      </ul>
     </div>
   );
 }
