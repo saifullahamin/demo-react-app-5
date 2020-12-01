19bc7e68b4a52e48c1f5d2157dc8ca38efce0805
Saifullah Amin <saifullahaminkhan@gmail.com>
2020-12-01 21:15:09 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "./App.css";
 function App() {
   const [repos, setRepos] = useState([{}]);
   useEffect(() => {
-
     // fetch("https://api.github.com/users/saifullahamin/repos")
     // .then((response) => response.json())
     // .then((json) => {
@@ -12,18 +11,18 @@ function App() {
     // });
 
     async function fetchData() {
-      const respone = await fetch(
+      const response = await fetch(
         "https://api.github.com/users/saifullahamin/repos"
       );
-      const data = await respone.json();
-      setRepos(data);
+      const json = await response.json();
+      setRepos(json);
     }
     fetchData();
   }, []);
 
   return (
     <div>
-      <h1>Github Repositories of github.com/saifullahamin</h1>
+      <h1>Github Repos for github.com/saifullahamin</h1>
       <ul>
         {repos.map((repoObj, id) => {
           return <li key={id}>{repoObj.name}</li>;
